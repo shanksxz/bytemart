746651957036f411111ab022e1d82724313b1ef2
somya bhatt <somyacu1@gmail.com>
2025-02-25 02:24:44 +0530

    ^checks

--- apps/server/src/config/index.ts
+++ apps/server/src/config/index.ts
@@ -2,17 +2,17 @@ import { z } from "zod";
 import "dotenv/config";
 
 const envSchema = z.object({
-	PORT: z.string().default("8787"),
+	SERVER_PORT: z.string().default("8787"),
 	DATABASE_URL: z.string().default(""),
-	JWT_SECRET: z.string().default(""),
-	ALLOWED_ORIGINS: z.string(),
+	SERVER_JWT_SECRET: z.string().default(""),
+	SERVER_ALLOWED_ORIGINS: z.string(),
 	CLIENT_URL: z.string().default("http://localhost:5173"),
 });
 
 const envVars = envSchema.parse(process.env);
 
-export const PORT = envVars.PORT;
+export const PORT = envVars.SERVER_PORT;
 export const DATABASE_URL = envVars.DATABASE_URL;
-export const JWT_SECRET = envVars.JWT_SECRET;
-export const ALLOWED_ORIGINS = envVars.ALLOWED_ORIGINS.split(",");
+export const JWT_SECRET = envVars.SERVER_JWT_SECRET;
+export const ALLOWED_ORIGINS = envVars.SERVER_ALLOWED_ORIGINS.split(",");
 export const CLIENT_URL = envVars.CLIENT_URL;
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import express from "express";
 import { PORT } from "./config";
+import { checkDatabaseConnection } from "@bytemart/database";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
+	checkDatabaseConnection();
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
